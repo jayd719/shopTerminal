e18a86d10a0bed64a00f58707d2c764a77c5ea21
JD-MBP <jashansingh29@gmail.com>
2023-10-22 00:51:43 -0400

    first commit

---
+++ src/com/shopTerminal/Item.java
@@ -0,0 +1,88 @@
+/**
+ *
+ */
+package com.shopTerminal;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+/**
+ *
+ */
+public class Item {
+
+    // Constants
+    /**
+     *
+     */
+    public static String SECTIONS[] = { "AREA!", "AREA1" };
+
+    // Attributes
+    private String name = null;
+    private int section = 0;
+
+    public Item(final String name, final int section) {
+	this.name = name;
+	this.section = section;
+	return;
+    }
+
+    /**
+     * @return Name of section of item
+     */
+    public String sectionString() {
+	return SECTIONS[section];
+    }
+
+    /**
+     * to display the object information.
+     */
+    @Override
+    public String toString() {
+	String returnString = String.format("Section:	%s\nSection(INT):	%d\nSection(STR):	%s", name,
+		section, SECTIONS[section]);
+	return returnString;
+    }
+
+    /**
+     * Makes sure file saved at same location irrespective of last //
+     *
+     * @param outputLocation
+     * @return
+     */
+    private String checkLocation(final String outputLocation) {
+	String location = "";
+	if (outputLocation != null) {
+	    if (outputLocation.charAt(outputLocation.length() - 1) == '/') {
+		location = outputLocation.substring(0, (outputLocation.length() - 1));
+		System.out.println(location);
+	    } else {
+		location = outputLocation;
+	    }
+	}
+	return location;
+    }
+
+    /**
+     * Saves Object Information in text file.
+     *
+     * @param outputLocation - where to save the file
+     * @return written - true if written/ false if failed to write.
+     */
+    public boolean toTxt(String outputLocation) {
+	boolean written = false;
+	outputLocation = this.checkLocation(outputLocation);
+	try {
+	    PrintStream outputFile = new PrintStream(new File(outputLocation + name + ".txt"));
+	    outputFile.println(this.toString());
+	    written = true;
+	} catch (FileNotFoundException e) {
+	    written = false;
+
+	}
+
+	return written;
+    }
+
+}
---
+++ src/com/shopTerminal/Main.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package com.shopTerminal;
+
+/**
+ * Main
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	Item I = new Item("Heela-1", 1);
+	System.out.println(I.toTxt("/Users/jashan/Desktop"));
+
+    }
+
+}
